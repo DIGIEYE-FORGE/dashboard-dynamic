75610318db36b8e39e8fa1401fd72108ec8140a5
oussama jamil <oussama@oussamas-MacBook-Pro.local>
2024-10-07 16:25:39 +0100

    delete header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { Outlet } from "react-router";
-import Header from "@/components/header";
-
 function App() {
   return (
     <main className=" flex flex-col">
-      <Header />
       <Outlet />
     </main>
   );
